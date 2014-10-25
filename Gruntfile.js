b84684caaf90e822deae59f8ab071cd144f0cf12
cgcgbcbc <cgcgbcbc@163.com>
2014-10-25 18:31:57 +0800

    init project with express cli with some custom
    Use swig instead of jade. Add initial Gruntfile.
    Add .npmrc in case that download Phantomjs failed.
    Add .editorconfig. Add .gitignore.

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = function(grunt) {
+
+    // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  grunt.registerTask('default', []);
+};
