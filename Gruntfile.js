8f572af83786ff348e16e4c0e3f282d8f2c64b4c
cgcgbcbc <cgcgbcbc@163.com>
2014-10-25 18:57:26 +0800

    add serve task.
    simply add express:dev to serve task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,36 @@
 'use strict'
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // Time how long tasks take. Can help when optimizing build times
+  grunt.loadNpmTasks('grunt-express-server');
+
+  // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    express: {
+      options: {
+        port: process.env.PORT || 9000
+      },
+      dev: {
+        options: {
+          script: 'bin/www',
+          debug: true
+        }
+      },
+      prod: {
+        options: {
+          script: 'dist/server/app.js'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('serve', function (target) {
+    grunt.task.run([
+      'express:dev'
+    ]);
+  });
 
   grunt.registerTask('default', []);
 };
