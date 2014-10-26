49d8ff8e7a3165bda9947bdf446655f8b53aef5f
cgcgbcbc <cgcgbcbc@163.com>
2014-10-27 01:50:04 +0800

    update api.spec

--- test/api.spec.js
+++ test/api.spec.js
@@ -6,6 +6,7 @@
 var should = require('should');
 var app = require('../app');
 var request = require('supertest');
+var id = 1;
 
 describe('Test API status', function() {
   it('should return ok GET /api/v1/status', function(done) {
@@ -16,29 +17,45 @@ describe('Test API status', function() {
   });
 });
 
-describe('POST /api/v1/treehole', function() {
+describe('POST /api/v1/holes', function() {
   it('should create tree-hole and returns id', function(done) {
     request(app)
-      .post('/api/v1/treehole')
+      .post('/api/v1/holes')
       .send({
-        "content": "test"
+        "title": "Test"
       })
-      .expect(200, done);
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .expect(function(res) {
+        res.body.should.have.property('id');
+        res.body.should.have.property('title', 'Test');
+        id = res.body.id;
+      })
+      .end(done);
   });
 });
 
-describe('GET /api/v1/treehole', function() {
+describe('GET /api/v1/holes', function() {
   it('should get tree-hole lists', function(done) {
     request(app)
-      .get('/api/v1/treehole')
-      .expect(200, done);
+      .get('/api/v1/holes')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(function(res) {
+        res.body.should.with.length(1);
+      })
+      .end(done);
   });
 });
 
-describe('GET /api/v1/treehole/:id', function() {
+describe('GET /api/v1/holes/:id', function() {
   it('should get a tree-hole', function(done) {
     request(app)
-      .get('/api/v1/treehole/1')
-      .expect(200, done);
+      .get('/api/v1/holes/' + id)
+      .expect(200)
+      .expect(function(res) {
+        res.body.should.have.property('title', 'Test');
+      })
+      .end(done);
   });
 });
