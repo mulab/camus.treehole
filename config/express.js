ba37e26715e85906d4f8a43c319905ebdd3d673b
cgcgbcbc <cgcgbcbc@163.com>
2014-10-28 00:21:49 +0800

    always mount routes even when db connection failed

--- config/express.js
+++ config/express.js
@@ -61,43 +61,42 @@ module.exports = function (app, callback) {
         req.db = db;
         next();
       });
+    } else {
+      console.log(err);
+    }
+    app.use('/', index);
+    app.use('/api/v1', api);
 
-      app.use('/', index);
-      app.use('/api/v1', api);
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
 
-      // catch 404 and forward to error handler
-      app.use(function (req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-      });
+    // error handlers
 
-      // error handlers
-
-      // development error handler
-      // will print stacktrace
-      if (app.get('env') === 'development') {
-        app.use(function (err, req, res, next) {
-          res.status(err.status || 500);
-          res.render('error', {
-            message: err.message,
-            error: err
-          });
-        });
-      }
-
-      // production error handler
-      // no stacktraces leaked to user
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
       app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
           message: err.message,
-          error: {}
+          error: err
         });
       });
-      callback();
-    } else {
-      console.log(err);
     }
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: {}
+      });
+    });
+    callback();
   });
 };
