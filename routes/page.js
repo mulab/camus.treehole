e7b867158211e7e8fbcc4aad99810e7b1d99f019
GengZhengLin <gzl8042@163.com>
2014-11-07 15:25:39 +0800

    http request

--- routes/page.js
+++ routes/page.js
@@ -2,10 +2,39 @@
 
 var express = require('express');
 var router = express.Router();
+var http = require("http");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+  var options = {
+    host: 'private-e37e7-treeholeapis.apiary-mock.com',
+    path: '/holes',
+    method: 'GET',
+    data: "{'number':'2'}",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  http.request(options, function(ress){
+    var temp;
+    ress.on('data', function (chunk) {
+      temp = JSON.parse(chunk);
+
+    });
+
+    ress.on('end', function () {
+      res.render('index', {
+        title: temp.toString(),
+        treeholes: temp
+      });
+      console.log(temp.toString());
+    });
+
+  }).end()
+
+
 });
+
 
 module.exports = router;
