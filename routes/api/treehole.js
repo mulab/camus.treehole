65959381f3c446a151949cee23d7ce0309be5a02
hqythu <hqythu@gmail.com>
2014-11-07 16:21:29 +0800

    post a new hole

--- routes/api/treehole.js
+++ routes/api/treehole.js
@@ -11,7 +11,15 @@ var db = require('../../config/db').db();
 function tree_hole(router) {
   //POST /treehole: create tree-hole
   router.post('/holes', function (req, res) {
-    db.collection('holes').insert({'title': req.body.title}, function (err, result) {
+    var hole = {};
+    hole.user = req.body.user;
+    hole.channel = req.body.channel;
+    hole.text = req.body.text;
+    hole.image = req.body.images;
+    hole.feedback_counters = [0, 0, 0];
+    hole.count_comments = 0;
+    hole.count_repost = 0;
+    db.collection('holes').insert(hole, function (err, result) {
       if (err) {
         console.log(err);
         throw err;
@@ -26,7 +34,6 @@ function tree_hole(router) {
     var number = req.param('number', 10);   //10 is the default value of number of holes to be retrieved
     if(number > 100)   //A maximum of 100 holes can be retrieved
       number = 100;
-    console.log(number);
     db.collection('holes').find({}, {limit : number}).toArray(function (err, docs) {
       if (err) {
         console.log(err);
