ed05e14dbb366f67978d74a4b839e7015ea8b82d
zmy <mengyu.chou@gmail.com>
2014-11-25 19:46:19 +0800

    use 'db_1' for temporary work

--- config/environments/production.js
+++ config/environments/production.js
@@ -7,7 +7,7 @@ var path = require('path');
 
 module.exports = {
   root: path.resolve(__dirname, '../../'),
-  db_host: 'db',
+  db_host: 'db_1',
   db_port: 27017,
   db_name: 'treeholes',
   db_option: {auto_reconnect: true},
