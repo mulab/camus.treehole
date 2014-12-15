32d271a54a7c3831021eaa43b16b4b35cb2be8b8
Zhipeng Jia <zhipeng.jia@outlook.com>
2014-12-16 02:03:35 +0800

    ready for WebStorm's debugger

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 'use strict';
 
-var child_process = require('child_process');
+var _ = require('lodash');
+var fork = require('child_process').fork;
+var runList = ['web/index.js', 'api/index.js'];
 
-var web = child_process.exec('node web/index.js');
+var debugPort = -1;
+var execArgv = [];
+_.each(process.execArgv, function (entry) {
+  var matchResult = entry.match(/--debug-brk=(\d+)/);
+  if (matchResult && matchResult[0] === entry) {
+    debugPort = parseInt(matchResult[1]);
+  } else {
+    execArgv.push(entry);
+  }
+});
 
-var api = child_process.exec('node api/index.js');
-
-web.stdout.pipe(process.stdout);
-api.stdout.pipe(process.stdout);
-web.stderr.pipe(process.stderr);
-api.stderr.pipe(process.stderr);
+_.each(runList, function (entry, i) {
+  if (debugPort !== -1) {
+    fork(entry, { execArgv: execArgv.concat('--debug-brk=' + (debugPort + i + 1)) });
+  } else {
+    fork(entry, { execArgv: execArgv });
+  }
+});
